3670ea258453f2ca0d83ea513916dc3f8d7a2922
sommergeo <26389306+sommergeo@users.noreply.github.com>
2020-01-18 13:31:09 +0100

    initial commit

---
+++ js/style_expressions.js
@@ -0,0 +1,206 @@
+function exp_Br1(context) {
+    // layer = 'Brain_size_data' AND "Age group" = 1
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return ((feature.properties['layer']  == 'Brain_size_data') && (feature.properties['Age group']  == 1));
+    } else {
+        return ((feature['layer']  == 'Brain_size_data') && (feature['Age group']  == 1));
+    }
+}
+
+
+function exp_Br2(context) {
+    // layer = 'Brain_size_data' AND "Age group" = 2
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return ((feature.properties['layer']  == 'Brain_size_data') && (feature.properties['Age group']  == 2));
+    } else {
+        return ((feature['layer']  == 'Brain_size_data') && (feature['Age group']  == 2));
+    }
+}
+
+
+function exp_Br3(context) {
+    // layer = 'Brain_size_data' AND "Age group" = 3
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return ((feature.properties['layer']  == 'Brain_size_data') && (feature.properties['Age group']  == 3));
+    } else {
+        return ((feature['layer']  == 'Brain_size_data') && (feature['Age group']  == 3));
+    }
+}
+
+
+function exp_Br4(context) {
+    // layer = 'Brain_size_data' AND "Age group" = 4
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return ((feature.properties['layer']  == 'Brain_size_data') && (feature.properties['Age group']  == 4));
+    } else {
+        return ((feature['layer']  == 'Brain_size_data') && (feature['Age group']  == 4));
+    }
+}
+
+
+function exp_Br5(context) {
+    // layer = 'Brain_size_data' AND "Age group" = 5
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return ((feature.properties['layer']  == 'Brain_size_data') && (feature.properties['Age group']  == 5));
+    } else {
+        return ((feature['layer']  == 'Brain_size_data') && (feature['Age group']  == 5));
+    }
+}
+
+
+function exp_Br6(context) {
+    // layer = 'Brain_size_data' AND "Age group" = 6
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return ((feature.properties['layer']  == 'Brain_size_data') && (feature.properties['Age group']  == 6));
+    } else {
+        return ((feature['layer']  == 'Brain_size_data') && (feature['Age group']  == 6));
+    }
+}
+
+
+function exp_Br7(context) {
+    // layer = 'Brain_size_data' AND "Age group" = 7
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return ((feature.properties['layer']  == 'Brain_size_data') && (feature.properties['Age group']  == 7));
+    } else {
+        return ((feature['layer']  == 'Brain_size_data') && (feature['Age group']  == 7));
+    }
+}
+
+
+function exp_Br8(context) {
+    // layer = 'Brain_size_data' AND "Age group" = 8
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return ((feature.properties['layer']  == 'Brain_size_data') && (feature.properties['Age group']  == 8));
+    } else {
+        return ((feature['layer']  == 'Brain_size_data') && (feature['Age group']  == 8));
+    }
+}
+
+
+function exp_Bo1(context) {
+    // layer = 'Body_size_data' AND "Age group" = 1
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return ((feature.properties['layer']  == 'Body_size_data') && (feature.properties['Age group']  == 1));
+    } else {
+        return ((feature['layer']  == 'Body_size_data') && (feature['Age group']  == 1));
+    }
+}
+
+
+function exp_Bo2(context) {
+    // layer = 'Body_size_data' AND "Age group" = 2
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return ((feature.properties['layer']  == 'Body_size_data') && (feature.properties['Age group']  == 2));
+    } else {
+        return ((feature['layer']  == 'Body_size_data') && (feature['Age group']  == 2));
+    }
+}
+
+
+function exp_Bo3(context) {
+    // layer = 'Body_size_data' AND "Age group" = 3
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return ((feature.properties['layer']  == 'Body_size_data') && (feature.properties['Age group']  == 3));
+    } else {
+        return ((feature['layer']  == 'Body_size_data') && (feature['Age group']  == 3));
+    }
+}
+
+
+function exp_Bo4(context) {
+    // layer = 'Body_size_data' AND "Age group" = 4
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return ((feature.properties['layer']  == 'Body_size_data') && (feature.properties['Age group']  == 4));
+    } else {
+        return ((feature['layer']  == 'Body_size_data') && (feature['Age group']  == 4));
+    }
+}
+
+
+function exp_Bo5(context) {
+    // layer = 'Body_size_data' AND "Age group" = 5
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return ((feature.properties['layer']  == 'Body_size_data') && (feature.properties['Age group']  == 5));
+    } else {
+        return ((feature['layer']  == 'Body_size_data') && (feature['Age group']  == 5));
+    }
+}
+
+
+function exp_Bo6(context) {
+    // layer = 'Body_size_data' AND "Age group" = 6
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return ((feature.properties['layer']  == 'Body_size_data') && (feature.properties['Age group']  == 6));
+    } else {
+        return ((feature['layer']  == 'Body_size_data') && (feature['Age group']  == 6));
+    }
+}
+
+
+function exp_Bo7(context) {
+    // layer = 'Body_size_data' AND "Age group" = 7
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return ((feature.properties['layer']  == 'Body_size_data') && (feature.properties['Age group']  == 7));
+    } else {
+        return ((feature['layer']  == 'Body_size_data') && (feature['Age group']  == 7));
+    }
+}
+
+
+function exp_Bo8(context) {
+    // layer = 'Body_size_data' AND "Age group" = 8
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return ((feature.properties['layer']  == 'Body_size_data') && (feature.properties['Age group']  == 8));
+    } else {
+        return ((feature['layer']  == 'Body_size_data') && (feature['Age group']  == 8));
+    }
+}
